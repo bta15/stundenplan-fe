15055ba4291194099eed766c2abd1c7f8ccc7460
bta15 <besha.taher74@gmail.com>
2024-10-10 13:22:54 +0200

    Weitere dependencies hinzugefügt

--- stundenplan/src/app/app.config.ts
+++ stundenplan/src/app/app.config.ts
@@ -1,9 +1,38 @@
-import {ApplicationConfig} from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, LOCALE_ID} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {MAT_DATE_LOCALE, MatNativeDateModule} from "@angular/material/core";
+import {ReactiveFormsModule} from "@angular/forms";
+import {registerLocaleData} from "@angular/common";
+import localeDe from '@angular/common/locales/de';
+import localeDeExtra from '@angular/common/locales/extra/de';
+import {provideMomentDateAdapter} from "@angular/material-moment-adapter";
+
+export const DATE_FORMAT = {
+  parse: {
+    dateInput: 'MMM YYYY',
+  },
+  display: {
+    dateInput: 'MMM YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMM YYYY',
+  },
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    provideAnimations(),
+    importProvidersFrom(MatNativeDateModule, ReactiveFormsModule),
+    provideMomentDateAdapter(DATE_FORMAT),
+    {provide: LOCALE_ID, useValue: 'de-DE'},
+    {provide: MAT_DATE_LOCALE, useValue: 'de-DE'}
+  ]
 };
+
+registerLocaleData(localeDe, 'de-DE', localeDeExtra);
